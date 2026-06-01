cd3f1555acef6eaf1de86fe4c5a6c52e8d2d958f
tmlyyy <thvics22@gmail.com>
2026-06-01 16:55:33 -0300

    Update ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: thamoliv <thamoliv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 19:45:59 by thamoliv          #+#    #+#             */
-/*   Updated: 2026/05/19 19:52:07 by thamoliv         ###   ########.fr       */
+/*   Updated: 2026/06/01 16:44:43 by thamoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,14 @@ void	*ft_calloc(size_t count, size_t size)
 	void	*p;
 	size_t	total_size;
 
+	if (count != 0 && size > SIZE_MAX / count)
+		return (NULL);
+
 	total_size = count * size;
 	p = malloc(total_size);
 	if (p == NULL)
 		return (NULL);
+
 	ft_bzero(p, total_size);
 	return (p);
 }
